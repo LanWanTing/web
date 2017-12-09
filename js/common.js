00e2fd67d061fbed4a8fd7ada60bb7b887021839
LanWanTing <roulita105@gmail.com>
2017-12-10 00:13:34 +0800

    20171210
    test

--- js/common.js
+++ js/common.js
@@ -1,12 +1,3 @@
-jQuery(document).ready(function() {
-	/* 平板旋轉設定 */
-	jQuery(window).on('orientationchange', function(e) {
-			jQuery(window).trigger('resize');
-	});	
-	jQuery(window).on('orientationchange', function(e) {
-    window.scroll(0, 0);
-  });
-  
-	jQuery('ul#menu').lavaLamp();  
+﻿jQuery(document).ready(function(e) {
 
 });
