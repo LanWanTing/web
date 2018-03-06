01bba0208f2db4422b1e16bc7e2952e108ba676e
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-03-06 23:50:04 +0800

    0306
    0306

--- myportfolio/js/list.js
+++ myportfolio/js/list.js
@@ -1,13 +1,5 @@
 $(function () {
     $('nav').load('nav.html');
-    //如果是手機版時() ==================================================================
-    /*if( $('.navbar-toggler').is(':visible') ){
-        //當選單按鈕被click
-        $('#navb .navbar-nav a:not(.dropdown-toggle)').click(function(){
-            //自動觸發.navbar-toggle按鈕被按=>選單關閉
-            window.location.href="网站链接";
-        });
-    }*/
     $('footer').load('footer.html');
 });
 
