4de95ad97d81a879d3e278aa6b0edd4b074d6449
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-03-06 22:57:09 +0800

    0306
    0306

--- myportfolio/js/list.js
+++ myportfolio/js/list.js
@@ -1,30 +1,28 @@
-$(function() {
+$(function () {
     $('nav').load('nav.html');
     $('footer').load('footer.html');
 });
 
 /*瀑布流效果*/
-$(function() {
+$(function () {
     /*$('.masonry').masonry({
 					  itemSelector: '.item'
 					});*/
     var $container = $('.masonry');
     //當圖片讀取完畢才執行
-    $container.imagesLoaded(function() {
+    $container.imagesLoaded(function () {
         // initialize
         $container.masonry({
             itemSelector: '.item'
         })
 
     });
-    /*
     //如果是手機版時() ==================================================================
-    if( $('navbar-toggler').is(':visible') ){
+    if( $('.navbar-toggler').is(':visible') ){
         //當選單按鈕被click
         $('#navb .navbar-nav a:not(.dropdown-toggle)').click(function(){
             //自動觸發.navbar-toggle按鈕被按=>選單關閉
             $('.navbar-toggler').trigger('click');
         });
     }
-    */
 });
