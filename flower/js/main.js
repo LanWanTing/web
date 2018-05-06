5a6a5b68bf1bf257c3e0bf6ba49598a5372384c3
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-05-06 15:55:52 +0800

    0506
    0506

--- flower/js/main.js
+++ flower/js/main.js
@@ -1,12 +1,26 @@
-$(function(){
+$(function () {
     var off = 1;
-    $('.navbar-toggler').click(function(){
-        if( off == 1 ){
+    $('.navbar-toggler').click(function () {
+        if (off == 1) {
             $('.navbar-toggler-icon').addClass('btnOn');
             off = 0;
-        }else{
+        } else {
             $('.navbar-toggler-icon').removeClass('btnOn');
             off = 1;
         }
     });
+    var viewW = $(window).width();
+    if (viewW > 992) {
+        $('.navbar-nav .nav-item').hover(function () {
+            if (off == 1) {
+                $('.dropdown-menu').addClass('show').fadeIn(300);
+                off = 0;
+            } else {
+                $('.dropdown-menu').removeClass('show').fadeOut(300);
+                off = 1;
+            }
+        });
+    }else{
+        $('.navbar-nav .nav-item').hover('style').hide();
+    }
 });
