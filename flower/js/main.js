7b8e803c918aad2e4bdd465c29bae71b28c47e87
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-05-06 22:47:18 +0800

    0506
    0506

--- flower/js/main.js
+++ flower/js/main.js
@@ -9,17 +9,31 @@ $(function () {
             off = 1;
         }
     });
-    $('.form-inline .btn').click(function () {
-        if (off == 1) {
+});
+/*視窗大於767 search直接顯示*/
+$(function () {
+    var off = 1;
+    var viewW = $(window).width();
+    if (viewW > 767) {
+        $('.form-inline .btn').click(function () {
+            if (off == 1) {
+                $('.form-control').addClass('formShow');
+                off = 0;
+            } else {
+                $('.form-control').removeClass('formShow');
+                off = 1;
+            }
+        });
+    } else {
+        $('.form-inline .btn').click(function(){
             $('.form-control').addClass('formShow');
-            $('.submitBtn').addClass('formShow');
-            off = 0;
-        } else {
-            $('.form-control').removeClass('formShow');
-            $('.submitBtn').removeClass('formShow');
-            off = 1;
-        }
-    });
+        });
+        $('.form-control').addClass('formShow')
+    }
+});
+/*視窗小於992 hover效果隱藏*/
+$(function () {
+    var off = 1;
     var viewW = $(window).width();
     if (viewW > 992) {
         $('.navbar-nav .nav-item').eq(1).hover(function () {
@@ -33,5 +47,5 @@ $(function () {
         });
     } else {
         $('.navbar-nav .nav-item').hover('style').hide();
-    };
+    }
 });
