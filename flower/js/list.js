7a89772acad9315e0e2cb881d3ddd182fd23cd5e
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-06-30 22:03:54 +0800

    0630
    0630

---
+++ flower/js/list.js
@@ -0,0 +1,9 @@
+/*
+$(function () {
+    $('.contentBox a').hover(function () {
+        $(this).children('.cover').stop(false, true).fadeIn(300);
+    }, function () {
+        $(this).children('.cover').stop(false, true).fadeOut(300);
+    });
+});
+*/
