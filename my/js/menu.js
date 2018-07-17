481ef28aeada8da57e6b910c51d486c6cebe0210
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-07-17 23:47:49 +0800

    0717
    0717

--- my/js/menu.js
+++ my/js/menu.js
@@ -1,17 +1,23 @@
 $(function(){
-    /*menu按鈕切換動態效果*/
+    /*menu按鈕切換時動態效果*/
     var off = 1;
     $('.menuBtn').click(function(){
         if (off == 1) {
             $('.menuBtn span').eq(0).addClass('lineA1');
             $('.menuBtn span').eq(1).addClass('lineB1');
             $('.menuBtn span').eq(2).addClass('lineC1');
+            $('#iMain').addClass('iMainH');
+            $('.menuWrap').stop(false, true).slideDown(500);
+            $('.navBox').stop(false, true).show();
             off = 0;
         }
         else{
             $('.menuBtn span').eq(0).removeClass('lineA1');
             $('.menuBtn span').eq(1).removeClass('lineB1');
             $('.menuBtn span').eq(2).removeClass('lineC1');
+            $('#iMain').removeClass('iMainH');
+            $('.menuWrap').stop(false, true).slideUp(500);
+            $('.navBox').stop(false, true).hide();
             off = 1;
         }
     });
