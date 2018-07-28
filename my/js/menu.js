62a8387a98d08eae49f356ed50115084d4e9e2a4
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-07-28 23:29:07 +0800

    0728
    0728

--- my/js/menu.js
+++ my/js/menu.js
@@ -6,7 +6,7 @@ $(function(){
             $('.menuBtn span').eq(0).addClass('lineA1');
             $('.menuBtn span').eq(1).addClass('lineB1');
             $('.menuBtn span').eq(2).addClass('lineC1');
-            $('#iMain').addClass('iMainH');
+            $('body').addClass('offScroll');
             $('.menuWrap').stop(false, true).slideDown(500);
             $('.navBox').stop(false, true).show();
             $('.menuTxt').stop(false, true).show();
@@ -16,11 +16,24 @@ $(function(){
             $('.menuBtn span').eq(0).removeClass('lineA1');
             $('.menuBtn span').eq(1).removeClass('lineB1');
             $('.menuBtn span').eq(2).removeClass('lineC1');
-            $('#iMain').removeClass('iMainH');
+            $('body').removeClass('offScroll');
             $('.menuWrap').stop(false, true).slideUp(500);
             $('.navBox').stop(false, true).hide();
             $('.menuTxt').stop(false, true).hide();
             off = 1;
         }
+    });
+    /*topBtn置頂按鈕*/
+    $(window).scroll(function () {
+        var scrollValue = $(window).scrollTop();
+        var targetScroll = $('.proList').offset().top;
+        if (scrollValue >= targetScroll) {
+            $('.topBtn').stop(true,true).fadeIn(1000);
+        } else {
+            $('.topBtn').stop(true,true).fadeOut(1000);
+        }
+    });
+    $('.topBtn').click(function(){
+        $('html,body').animate({scrollTop:0} ,1500 ,'easeOutCirc');
     });
 });
