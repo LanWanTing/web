aad244e184fdb497848bcc1c2259da10c03917ba
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-08-05 17:08:21 +0800

    0805
    0805

--- my/js/gallery.js
+++ my/js/gallery.js
@@ -1,12 +1,11 @@
+/*
 $(function () {
-    $(".workPhoto").delegate("li", "click", function () {
+    $(".workApp").delegate("li", "click", function () {
         if (document.body.scrollTop == 0) {
             var wrapTop = document.documentElement.scrollTop;
         } else {
             var wrapTop = document.body.scrollTop;
         }
-        $('body').addClass('offScroll');
-        //$(".iMain").hide();
         $(".workGallery").stop(true, true).fadeIn(500);
         $(".workGallery").find("." + $(this).attr("org")).stop(true, true).fadeIn(500);
         $("html, body").scrollTop(0);
@@ -15,32 +14,38 @@ $(function () {
             $("html, body").scrollTop(wrapTop);
             $('body').removeClass('offScroll');
         });
+        $(".workGallery").click(function () {
+            $(this).stop(true, true).fadeOut(500);
+            $("html, body").scrollTop(wrapTop);
+            $('body').removeClass('offScroll');
+        });
     });
 });
+*/
 
 $(function () {
-    //變數宣告區=============================================================
-    var galleryBoxNo = -1; //負責page1目前畫面呈現的背景的索引號碼(0代表第1張)
-
-    //page1影像輪播----------------------------------------------------------
-    //偵測.page1中class名稱為page1bg的length"總數量"
-    //alert($('.page1 .page1bg').length);
-
-    function page1show() {
-        //讓目前畫面上的影像以1秒的時間淡出
-        $('.workGallery .galleryBox').eq(galleryBoxNo).fadeOut(0);
-
-        //準備下一張
-        galleryBoxNo++;
-        if (galleryBoxNo == $('.page1 .page1bg').length) {
-            galleryBoxNo = 0;
-        }
-
-        //讓下一張以1秒的時間淡入
-        $('.workGallery .galleryBox').eq(galleryBoxNo).fadeIn(1000);
-    }
-
-    page1show(); //馬上執行一次page1show()函式
-    setInterval(page1show, 2000); //設定每隔5秒之後執行一次page1show的函式
-
+    $('.appBtn1').click(function(){
+        $('.app1').stop(true,true).show();
+        $('.app2').stop(true,true).hide();
+        $('.app3').stop(true,true).hide();
+        $('.app4').stop(true,true).hide();
+    });
+    $('.appBtn2').click(function(){
+        $('.app1').stop(true,true).hide();
+        $('.app2').stop(true,true).show();
+        $('.app3').stop(true,true).hide();
+        $('.app4').stop(true,true).hide();
+    });
+    $('.appBtn3').click(function(){
+        $('.app1').stop(true,true).hide();
+        $('.app2').stop(true,true).hide();
+        $('.app3').stop(true,true).show();
+        $('.app4').stop(true,true).hide();
+    });
+    $('.appBtn4').click(function(){
+        $('.app1').stop(true,true).hide();
+        $('.app2').stop(true,true).hide();
+        $('.app3').stop(true,true).hide();
+        $('.app4').stop(true,true).show();
+    });
 });
