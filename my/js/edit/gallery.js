7f6e56507ede48a6954a02aa728ba4365c508d8b
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-08-06 23:41:30 +0800

    0806
    0806

--- my/js/edit/gallery.js
+++ my/js/edit/gallery.js
@@ -1,3 +1,4 @@
+/*APP圖片切換*/
 $(function () {
     $('.appBtn1').click(function(){
         $('.app1').stop(true,true).fadeIn(500);
@@ -22,5 +23,19 @@ $(function () {
         $('.app1').stop(true,true).fadeOut(500);
         $('.app2').stop(true,true).fadeOut(500);
         $('.app3').stop(true,true).fadeOut(500);
+    });
+});
+
+
+/*APP圖片按鈕顯示設定*/
+$(function () {
+    $(window).scroll(function () {
+        var scrollValue = $(window).scrollTop();
+        var targetScroll = $('.workTxtContent').offset().top;
+        if (scrollValue >= targetScroll) {
+            $('.workApp').stop(true,true).fadeIn(1000);
+        } else {
+            $('.workApp').stop(true,true).fadeOut(1000);
+        }
     });
 });
