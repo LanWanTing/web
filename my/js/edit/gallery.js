44c739907e0132cc802292b448f0d18ecaa3d928
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-08-09 23:40:12 +0800

    0809
    0809

--- my/js/edit/gallery.js
+++ my/js/edit/gallery.js
@@ -1,41 +1,50 @@
 /*APP圖片切換*/
 $(function () {
-    $('.appBtn1').click(function(){
-        $('.app1').stop(true,true).fadeIn(500);
-        $('.app2').stop(true,true).fadeOut(500);
-        $('.app3').stop(true,true).fadeOut(500);
-        $('.app4').stop(true,true).fadeOut(500);
+    $('.appBtn1').click(function () {
+        $('.app1').stop(true, true).fadeIn(300);
+        $('.app2').stop(true, true).fadeOut(300);
+        $('.app3').stop(true, true).fadeOut(300);
+        $('.app4').stop(true, true).fadeOut(300);
     });
-    $('.appBtn2').click(function(){
-        $('.app2').stop(true,true).fadeIn(500);
-        $('.app1').stop(true,true).fadeOut(500);
-        $('.app3').stop(true,true).fadeOut(500);
-        $('.app4').stop(true,true).fadeOut(500);
+    $('.appBtn2').click(function () {
+        $('.app2').stop(true, true).fadeIn(300);
+        $('.app1').stop(true, true).fadeOut(300);
+        $('.app3').stop(true, true).fadeOut(300);
+        $('.app4').stop(true, true).fadeOut(300);
     });
-    $('.appBtn3').click(function(){
-        $('.app3').stop(true,true).fadeIn(500);
-        $('.app1').stop(true,true).fadeOut(500);
-        $('.app2').stop(true,true).fadeOut(500);
-        $('.app4').stop(true,true).fadeOut(500);
+    $('.appBtn3').click(function () {
+        $('.app3').stop(true, true).fadeIn(300);
+        $('.app1').stop(true, true).fadeOut(300);
+        $('.app2').stop(true, true).fadeOut(300);
+        $('.app4').stop(true, true).fadeOut(300);
     });
-    $('.appBtn4').click(function(){
-        $('.app4').stop(true,true).fadeIn(500);
-        $('.app1').stop(true,true).fadeOut(500);
-        $('.app2').stop(true,true).fadeOut(500);
-        $('.app3').stop(true,true).fadeOut(500);
+    $('.appBtn4').click(function () {
+        $('.app4').stop(true, true).fadeIn(300);
+        $('.app1').stop(true, true).fadeOut(300);
+        $('.app2').stop(true, true).fadeOut(300);
+        $('.app3').stop(true, true).fadeOut(300);
+        $('.app3').stop(true, true).fadeOut(300);
     });
 });
 
 
 /*APP圖片按鈕顯示設定*/
 $(function () {
-    $(window).scroll(function () {
-        var scrollValue = $(window).scrollTop();
-        var targetScroll = $('.workTxtContent').offset().top;
-        if (scrollValue >= targetScroll) {
-            $('.workApp').stop(true,true).fadeIn(1000);
-        } else {
-            $('.workApp').stop(true,true).fadeOut(1000);
-        }
+    var viewW = $(window).width();
+    if (viewW <= 768) {
+        $(window).scroll(function () {
+            var scrollValue = $(window).scrollTop();
+            var targetScroll = $('.workTxtContent').offset().top;
+            if (scrollValue >= targetScroll) {
+                $('.workApp').stop(true, true).fadeIn(1000);
+            } else {
+                $('.workApp').stop(true, true).fadeOut(1000);
+            }
+        });
+    } else {
+        $('.workApp').show();
+    }
+    $(window).resize(function(){
+       location.reload(); 
     });
 });
